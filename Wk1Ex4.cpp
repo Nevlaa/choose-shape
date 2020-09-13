fda4c88c1d0018dd2c93de238c9ada8617138e8c
Scott horsburgh <shorsburgh19@gmail.com>
2020-09-12 23:46:31 -0600

    adding files

---
+++ Wk1Ex4.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+    double num1 = 75.35;
+	double num2 = -35.56;
+	double num3 = 15.76;
+	double sum = num1 + num2 + num3;
+    double average = sum / 3;
+	
+int main()
+{
+	cout << " Num1 = " << num1 << endl;
+	cout << " Num2 = " << num2 << endl;
+	cout << " Num3 = " << num3 << endl;
+	cout << " Average = " << average << endl;
+	
+	return 0; 
+}
